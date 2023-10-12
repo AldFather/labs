3e1beb9b67db86e8b339455dcaf5b920a03badcd
AldFather <146945501+AldFather@users.noreply.github.com>
2023-10-12 13:41:35 +0300

    Add files via upload

---
+++ repos/4.2/4.2/Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include <iomanip>
+
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+    
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+
+	x = xp; 
+	while (x <= xk)
+	{
+		A = (x * x) * cos(x / 2);
+		if (x < -5)
+			B = (x / 5) - exp(abs(x));
+		else
+			if (-5 <= x && x <= 0)
+				B = ((x * x * x) / 4) + 1;
+			else
+				B = 1. / tan(log(x));
+
+	y = A + B;
+	
+	cout << "|" << setw(7) << setprecision(2) << x
+		<< " |" << setw(10) << setprecision(3) << y
+		<< " |" << endl;
+	
+	x += dx;
+	
+	}
+	cout << "---------------------------" << endl;
+	
+	return 0;
+}
