71c5cf16cf27bb5279358fadb02fdf398b16d734
AldFather <146945501+AldFather@users.noreply.github.com>
2023-12-07 11:52:42 +0200

    Add files via upload

---
+++ repos/8.1.рек/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "CppUnitTest.h"
+#include "../8.1.рек/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(CountFunction_ShouldReturnCorrectCount)
+        {
+            char str[] = "aa bb crs aa bb aa";
+            int count = Count(str, 1);
+            Assert::AreEqual(5, count);
+        }
+
+        TEST_METHOD(ChangeFunction_ShouldModifyStringCorrectly)
+        {
+            char str[] = "aa db cc ff rs";
+            char dest[151];
+            dest[0] = '\0';
+            char* modifiedStr = Change(dest, str, dest, 0);
+
+            Assert::AreEqual("*** db *** ff rs", modifiedStr);
+        }
+    };
+}
